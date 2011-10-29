c3fc000fdaab203e87dfb67e6a21989bd49ba62f
Gustavo Garcia <goostavobg@gmail.com>
2011-10-29 02:21:10 -0200

    -Adicionado arquivo para buffer circular -Melhorado esqueleto do programa -adicionada função para leitura de entrada

---
+++ histbuf.h
@@ -0,0 +1,8 @@
+//Buffer circular para guardar o histórico
+#ifndef _HISTBUF_H_
+#define _HISTBUF_H_
+
+
+#endif
+
+
--- main.c
+++ main.c
@@ -3,24 +3,43 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include "histbuf.h"
 #include "parser.h"
 #include "comandos.h"
 
+//Funções básicas do sistema
 void inicializa(char* diretorio);
+void le_entrada(char* entrada, char* diretorio);
 
 int main(void)
 {
-    char diretorio[1024];
+    int condicao_saida = 0;
+    char diretorio[1024];   //Diretorio corrente
+    char entrada[1024];     //Entrada feito pelo usuário
     inicializa(diretorio);
-    //parse();
-    //proccess();
+    //Loop para realizar interações
+    while (condicao_saida == 0)
+    {
+        le_entrada(entrada,diretorio);
+        //parse();
+        //proccess();
+    }
 
     return 0;
 }
 
-//Função que inicializa o sistema.
+//Inicializa variáveis e imprime mensagem inicial
 void inicializa(char* diretorio)
 {
     printf("\nBem vindo a Shii - Shell Interativa Infima para iniciantes!\n");
     getcwd(diretorio,1024);
+}
+
+//Mostra cabeçalho e le entrada de usuário
+//entrada -> valor digitado pelo usuário
+//diretorio -> diretorio corrente
+void le_entrada(char* entrada, char* diretorio)
+{
+    printf("Shiii:%s$ ",diretorio);
+    gets(entrada);
 }
