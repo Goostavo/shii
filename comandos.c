f64ad79c3ab8b364c3ab20b90a4392c5012e0a93
Vinicius Lana <viniciuslana.viveiros@gmail.com>
2011-10-29 02:14:13 -0300

    Adicionado os comandos: -cd -pwd -wait

--- comandos.c
+++ comandos.c
@@ -1,1 +1,29 @@
 #include "comandos.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void command_pwd(void)
+{
+    char cwd[1024];
+    getcwd(cwd,sizeof(cwd));
+    printf("%s\n",cwd);
+}
+
+
+void command_wait(void)
+{
+    pid_t pid;
+    pid = waitpid(-1,0,0);
+    while (pid != -1)
+    {
+        printf("Process %d Ended\n",pid);
+        pid = waitpid(-1,0,0);
+    }
+}
+
+void command_cd(char *diretorio)
+{
+    chdir(diretorio);
+}
--- comandos.h
+++ comandos.h
@@ -1,6 +1,20 @@
 //Comandos do shell
 #ifndef _COMANDOS_H_
 #define _COMANDOS_H_
+//Esta função exibi o diretório corrente da shell
+//Parametros de entrada : sem parametros
+//Valor de retorno : Não retorna valor
+void command_pwd(void);
 
+//Esta função faz com que a shell espere o término de todos os processos em execução
+//Assim que os processos vão sendo finalizados, são exibidos os pid's dos mesmos.
+//Parametros de entrada : sem parametros
+//Valor de retorno : Não retorna valor
+void command_wait(void);
+
+//Esta função muda o diretório corrente da shell
+//Parametros de entrada : Caminho do Diretório
+//Valor de retorno : Não retorna valor
+void command_cd(char *diretorio);
 
 #endif
